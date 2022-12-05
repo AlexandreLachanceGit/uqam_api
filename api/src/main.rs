2bb99d5657598b95cccdd4c204c2eb201fa33663
Lachance, Alexandre <alexandrelachance@me.com>
2022-12-05 15:56:15 -0500

    Setup api project.

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use lambda_http::{run, service_fn, Body, Error, Request, RequestExt, Response};
+use serde_json::json;
+
+async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
+    println!("{:?}", event);
+
+    let resp = Response::builder()
+        .status(200)
+        .header("content-type", "application/json")
+        .body(json!({
+            "test": "value"
+        }).to_string().into())
+        .map_err(Box::new)?;
+    Ok(resp)
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::INFO)
+        // disable printing the name of the module in every log line.
+        .with_target(false)
+        // disabling time is handy because CloudWatch will add the ingestion time.
+        .without_time()
+        .init();
+
+    run(service_fn(function_handler)).await
 }
